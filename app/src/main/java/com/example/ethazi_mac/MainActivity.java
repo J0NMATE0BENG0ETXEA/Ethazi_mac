e476650559eb90dd24edf1bbc29140877fec3e21
J0NMATE0BENG0ETXEA <videojuegos2000>
2020-01-16 10:11:45 +0100

    Login Estruktura

--- app/src/main/java/com/example/ethazi_mac/MainActivity.java
+++ app/src/main/java/com/example/ethazi_mac/MainActivity.java
@@ -5,13 +5,76 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtErabiltzaile, txtPasahitza;
+    Button btnSartu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtErabiltzaile=(EditText)findViewById(R.id.editTextUser);
+        txtPasahitza=(EditText)findViewById(R.id.editTextPass);
+        btnSartu=(Button)findViewById(R.id.buttonSartu);
+
+        btnSartu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                validarUsuario("http://192.168.13.26/ethazi_mac/validar_usuario.php");
+            }
+        });
+
     }
+
+    public void validarUsuario(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(!response.isEmpty()){
+                    //Intent
+                    Toast.makeText(MainActivity.this, "Bien login", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //Toast error login
+                    Toast.makeText(MainActivity.this, "Mal login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //TOAST capturar error
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        } ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros=new HashMap<String, String>();
+                parametros.put("usuario", txtErabiltzaile.getText().toString());
+                parametros.put("password", txtPasahitza.getText().toString());
+                return parametros;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
 
 }
