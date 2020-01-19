62718036755efb09e22c59084b37fe415103a716
J0NMATE0BENG0ETXEA <46562628+J0NMATE0BENG0ETXEA@users.noreply.github.com>
2020-01-19 19:28:17 +0100

    MAPBOX, Maparen funtzioak

--- app/src/main/java/com/example/ethazi_mac/InfoPantalla.java
+++ app/src/main/java/com/example/ethazi_mac/InfoPantalla.java
@@ -2,15 +2,19 @@ package com.example.ethazi_mac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class InfoPantalla extends AppCompatActivity {
 
     private Ostatu ostatu;
-    private String latidude, longitude;
+    private String latidude, longitude, izena, desk;
     private TextView tvizena, tvdesk, tvmota, tvkokapena, tvtelefonoa, tvemail;
+    private Button btnMapa, btnReserva;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +33,27 @@ public class InfoPantalla extends AppCompatActivity {
 
         //Textua ezarri
         tvizena.setText((String) bundle.get("izena"));
+        izena = (String) bundle.get("izena");
         tvdesk.setText((String) bundle.get("desk"));
+        desk = (String) bundle.get("desk");
         tvdesk.setMovementMethod(new ScrollingMovementMethod());
         tvmota.setText((String) bundle.get("mota"));
         tvkokapena.setText((String) bundle.get("kokapena"));
         tvtelefonoa.setText((String) bundle.get("telefono"));
         tvemail.setText((String) bundle.get("email"));
-
-
+        latidude = (String) bundle.get("latitude");
+        longitude = (String) bundle.get("longitude");
     }
 
+    //VER MAPA
+    public void ikusiMapa(View view){
+        Intent i = new Intent(this, mapa.class );
+        i.putExtra("longi", longitude);
+        i.putExtra("lati", latidude);
+        i.putExtra("izen", izena);
+        i.putExtra("deskrip", desk);
+        startActivity(i);
 
+    }
 
 }
---
+++ app/src/main/java/com/example/ethazi_mac/mapa.java
@@ -0,0 +1,117 @@
+package com.example.ethazi_mac;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+public class mapa extends AppCompatActivity {
+    private MapView mapaView;
+    private String longitude, latitude, izena, desk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, "sk.eyJ1IjoiYmFzYXRvciIsImEiOiJjazVncWdicnAwOW5xM2ZwZTJ1OXQ0emgzIn0.y2afr5eNZWb7nByh97d3ZQ");
+        setContentView(R.layout.activity_mapa);
+        getSupportActionBar().hide();
+        mapaView = (MapView) findViewById(R.id.mapView);
+        mapaView.onCreate(savedInstanceState);
+        mapaView.getMapAsync(new OnMapReadyCallback(){
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style){
+
+                    }
+                });
+
+
+            }
+        });
+        //Beste activity informazioa
+        Bundle bundle = getIntent().getExtras();
+        izena = (String) bundle.get("izen");
+        desk = (String) bundle.get("deskrip");
+        latitude = (String) bundle.get("lati");
+        longitude = (String) bundle.get("longi");
+
+        //Konbertsioak
+        final Double lati, longi;
+        lati = Double.parseDouble(latitude);
+        longi = Double.parseDouble(longitude);
+
+        //MAPBOX
+        mapaView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(
+                                lati, longi))//Cogemos de la BD las coordenadas del alojamiento que se ha escogido
+                        .title(izena)//Cogemos de la BD el nombre del alojamiento que se ha escogido
+                        .snippet(desk));//Cogemos de la BD la descripcion del alojamiento que se ha escogido
+
+                CameraPosition position = new CameraPosition.Builder()
+                        .target(new LatLng(lati, longi)) // Cogemos de la BD las coordenadas del alojamiento que se ha escogido
+                        .zoom(17) // Fija el nivel de zoom
+                        .tilt(30) // Fija la inclinación de la cámara
+                        .build();
+                mapboxMap.animateCamera(CameraUpdateFactory
+                        .newCameraPosition(position), 7000);
+            }
+        });
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        mapaView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapaView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapaView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapaView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapaView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapaView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapaView.onSaveInstanceState(outState);
+    }
+}
