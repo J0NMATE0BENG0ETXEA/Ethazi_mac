aee20821f5d771fa30b4f7c121d8215bef0315e4
J0NMATE0BENG0ETXEA <videojuegos2000>
2020-01-20 14:09:49 +0100

    kodigo txarra

--- app/src/main/java/com/example/ethazi_mac/Erregistratu.java
+++ app/src/main/java/com/example/ethazi_mac/Erregistratu.java
@@ -3,12 +3,46 @@ package com.example.ethazi_mac;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Erregistratu extends AppCompatActivity {
+    private EditText txtizenabizen, txterabiltzaile, txtpasahitza1, txtpasahitza2, txtmail, txttelefono;
+    private Button btnReserva;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_erregistratu);
+
+        txterabiltzaile=(EditText)findViewById(R.id.editTextuser);
+        txtpasahitza1=(EditText)findViewById(R.id.editPass1);
+        txtpasahitza2=(EditText)findViewById(R.id.editPass2);
+        txtizenabizen=(EditText)findViewById(R.id.editTextizenabizen);
+        txtmail=(EditText)findViewById(R.id.editTextmail);
+        txttelefono=(EditText)findViewById(R.id.editTexttelefonoa);
+        btnReserva=(Button) findViewById(R.id.buttonErregis);
     }
+
+
+    public void DatuakOndo(){
+
+                    if(TextUtils.isEmpty((CharSequence) txtizenabizen) && TextUtils.isEmpty((CharSequence) txterabiltzaile) && TextUtils.isEmpty((CharSequence) txtpasahitza1) && TextUtils.isEmpty((CharSequence) txtpasahitza2)) {
+                        txtizenabizen.setError("");
+                        txterabiltzaile.setError("");
+                        txtpasahitza1.setError("");
+                        txtpasahitza2.setError("");
+                        return;
+                    }
+                    else{
+                        if (txtpasahitza1.getText().equals(txtpasahitza2.getText())){
+
+                        }
+                        else {
+                            //CODIGO PARA AVANZAR
+                        }
+                    }
+                }
 }
